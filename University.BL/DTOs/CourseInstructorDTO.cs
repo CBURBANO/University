781723a10c2523e1ca47f8dc84b5097e1a0500f4
Carolina Burbano <carotecno5@gmail.com>
2021-09-05 21:47:04 -0500

    Ingreso de DTOSs

---
+++ University.BL/DTOs/CourseInstructorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.BL.DTOs
+{
+    public class CourseInstructorDTO
+    {
+        [Required()]
+        public int ID { get; set; }
+        [Required()]
+        public int CourseID { get; set; }
+        [Required()]
+        public int InstructorID { get; set; }
+
+        //nav
+        public CourseDTO Course { get; set; }
+        public InstructorDTO Instructor { get; set; }
+    }
+}
---
+++ University.BL/DTOs/DepartmentDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.BL.DTOs
+{
+    public class DepartmentDTO
+    {
+        [Required()]
+        public int DepartmentID { get; set; }
+
+        [Required()]
+        public string Name { get; set; }
+
+        [Required()]
+        public decimal Budget { get; set; }
+
+        [Required()]
+        public DateTime StartDate { get; set; }
+
+        //nav
+        public int InstructorID { get; set; }
+
+        public InstructorDTO Instructor { get; set; }
+
+    }
+}
